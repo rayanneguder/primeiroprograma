b2bf62d0c7164c2e3d96c75d57aba5893be00356
rayanneguder <rayanne.guder.rodrigues@escola.pr.gov.br>
2025-02-27 12:04:19 -0300

    Add files via upload

---
+++ raynao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int numero = 5;
+  int chute; 
+  printf("digite um numero: ");
+   scanf("%d", &chute);
+   
+   if(chute == numero){ 
+   printf("acertou! \n");
+}else{
+    printf("errou, bobão! \n");
+}
+  printf("o numero era %d", numero); 
+ return 0;
+}
